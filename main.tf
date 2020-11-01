7d27deedfc7152df4fb2ac3aac64771f23625060
sab <charlespogzz@googlemail.com>
2020-11-02 07:32:08 +0800

    matulog nko

--- main.tf
+++ main.tf
@@ -62,8 +62,8 @@ resource "aws_autoscaling_group" "ASGgroup" {
     target_group_arns = [aws_lb_target_group.asgTG.arn]
     health_check_type = "ELB"
 
-    count =  = 3
-    desired_capacity=2
+    count = 3
+    desired_capacity = 2
     min_size = 1
     max_size = 4
 
