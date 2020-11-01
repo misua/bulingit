8d2e5c9d9f7973746f1e5a1c06509c4ce15afb94
sab <charlespogzz@googlemail.com>
2020-11-02 06:34:58 +0800

    pistiii kpoy nko

--- main.tf
+++ main.tf
@@ -100,8 +100,8 @@ resource "aws_security_group" "albSG" {
     }
 
     egress {
-        from_port = 80
-        to_port = 80
+        from_port = 0
+        to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
